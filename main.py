65bc16977724ad51bd5a3bfddad3311a55af8474
Paul Magalong <paul.magalong0@gmail.com>
2026-09-03 18:13:18 +0800

    simulation

--- main.py
+++ main.py
@@ -3,11 +3,4 @@ print('Author: Paul Charles B. Magalong')
 print('Student ID: TUPM-26-1134')
 print('Class Section: BSECE - 1C')
 
-print('Login Feature Enabled')
-
-print('Lab 1: Version Control')
-print('Author: Paul Charles B. Magalong')
-print('Student ID: TUPM-26-1134')
-print('Class Section: BSECE - 1C')
-
-print('Login Feature Enabled')
+print('Login Feature Neutral')
